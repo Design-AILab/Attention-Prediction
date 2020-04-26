2d2294bf3e304e8d335caab4df94a08391c3e5ba
FZQ <furankyyy@yahoo.com>
2020-04-26 12:28:55 -0300

    Add files via upload
    Need ##prediction

---
+++ Frank/app.py
@@ -0,0 +1,28 @@
+import flask
+import os
+from werkzeug.utils import secure_filename
+
+app = flask.Flask(__name__, template_folder='templates')
+
+@app.route('/',methods=['GET', 'POST'])
+
+def home():
+    if flask.request.method == 'POST':
+        file = flask.request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('uploads', filename))
+        return flask.redirect(flask.url_for('prediction', filename=filename))
+    return(flask.render_template('home.html'))
+
+
+@app.route('/prediction/<filename>')
+def prediction(filename):
+
+    image = plt.imread(os.path.join('uploads', filename))
+
+    ### code for prediction
+    
+    return render_template('predict.html', predictions=predictions)
+
+
+app.run()
